8e72d00b40cc18ebe5f71e577e218c7132e72da9
Eli Gundry <eligundry@gmail.com>
2013-09-16 18:40:28 -0400

    Added some skeleton functions for week 4

---
+++ Week-04/anamorphic.cpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  anamorphic.cpp
+ *
+ *    Description:  Anamorphic speed programming challenge
+ *					http://www.cs.kent.edu/~rothstei/fall_13/sep16/anamorphic.pdf
+ *
+ *        Version:  1.0
+ *        Created:  09/16/2013 06:19:43 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eli Gundry
+ *
+ * =====================================================================================
+ */
+
+#include <cstdio>
+#include <iostream>
+
+void morph(const int id[], const int od[])
+{
+}
+
+int main(void)
+{
+	int input_dimensions[2],
+		output_dimensions[2];
+
+	std::cout << "Input input dimensions" << std::endl;
+
+	return 0;
+}
---
+++ Week-04/elevator.cpp
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  elevator.cpp
+ *
+ *    Description:  Elevator speed programming problem
+ *					http://www.cs.kent.edu/~rothstei/fall_13/sep16/elevator.pdf
+ *
+ *        Version:  1.0
+ *        Created:  09/16/2013 05:44:30 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eli Gundry
+ *
+ * =====================================================================================
+ */
+
+#include <cstdio>
+#include <iostream>
+
+void move_floors(const int& floors, int& current_floor, int floor_movement[])
+{
+}
+
+int main(void)
+{
+	int floors = 0,
+		current_floor = 0,
+		floor_movement[20];
+
+	std::cin >> floors;
+
+	do {
+	} while (floor_movement[0]);
+
+	return 0;
+}
