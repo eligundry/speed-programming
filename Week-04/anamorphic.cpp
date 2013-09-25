d54f7a087f2078aeed295c651751eba5b0c9aed3
Eli Gundry <eligundry@gmail.com>
2013-09-25 16:56:39 -0400

    Started week 5

--- Week-04/anamorphic.cpp
+++ Week-04/anamorphic.cpp
@@ -16,19 +16,44 @@
  * =====================================================================================
  */
 
-#include <cstdio>
 #include <iostream>
+#include <cstdio>
+#include <cmath>
 
-void morph(const int id[], const int od[])
+void morph(const double src[], const double tar[])
 {
+	double results[2],
+		   aspect_ratio[2];
+
+	// source aspect ratio
+	aspect_ratio[0] = src[1] / src[0];
+
+	// target aspect ratio
+	aspect_ratio[1] = tar[1] / tar[0];
+
+	printf("%f, %f\n", aspect_ratio[0], aspect_ratio[1]);
+
+	if (aspect_ratio[0] >= aspect_ratio[1]) {
+		results[0] = tar[0];
+		results[1] = floor((tar[0] * src[1]) / src[0]);
+	} else {
+		results[1] = tar[1];
+		results[0] = ceil((tar[1] * src[0]) / src[1]);
+	}
+
+	printf("w x h = %f x %f pixels\n", results[0], results[1]);
 }
 
 int main(void)
 {
-	int input_dimensions[2],
-		output_dimensions[2];
+	double source[2],
+		   target[2];
 
-	std::cout << "Input input dimensions" << std::endl;
+	std::cin >> source[0] >> source[1] >> target[0] >> target[1];
+
+	printf("(%fx%f) (%fx%f)\n", source[0], source[1], target[0], target[1]);
+
+	morph(source, target);
 
 	return 0;
 }
---
+++ Week-05/quodigious.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  quodigious.cpp
+ *
+ *    Description:  http://www.cs.kent.edu/~rothstei/fall_13/sep23/quodigious.pdf
+ *
+ *        Version:  1.0
+ *        Created:  09/25/2013 04:44:22 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Eli Gundry
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+void getQD(int in)
+{
+}
+
+int main(void)
+{
+	int input = 0;
+
+	while (std::cin >> input) {
+		getQD(input);
+	}
+
+	return 0;
+}
